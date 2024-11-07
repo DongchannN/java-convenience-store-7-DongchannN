45f151f469114ddf38785fb0ccf214890f1c08ee
DongchannN <ldc991104@gmail.com>
2024-11-07 19:22:47 +0900

    feat: 프로모션 관리 모델 구현

---
+++ src/main/java/store/model/Promotion.java
@@ -0,0 +1,33 @@
+package store.model;
+
+import java.time.LocalDateTime;
+
+public class Promotion {
+    private final String name;
+    private final int buyAmount;
+    private final int giveAmount;
+    private final LocalDateTime startAt;
+    private final LocalDateTime endAt;
+
+    public Promotion(String name, int buyAmount, int giveAmount, LocalDateTime startAt, LocalDateTime endAt) {
+        validateBuyAmount(buyAmount);
+        validateGiveAmount(giveAmount);
+        this.name = name;
+        this.buyAmount = buyAmount;
+        this.giveAmount = giveAmount;
+        this.startAt = startAt;
+        this.endAt = endAt;
+    }
+
+    private void validateBuyAmount(int buyAmount) {
+        if (buyAmount < 1) {
+            throw new IllegalArgumentException("프로모션 구매 개수는 0 이상이여야 합니다.");
+        }
+    }
+
+    private void validateGiveAmount(int giveAmount) {
+        if (giveAmount != 1) {
+            throw new IllegalArgumentException("프로모션 증정 개수는 1 이여야 합니다.");
+        }
+    }
+}
