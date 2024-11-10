2e8bae2edd71b7e93446ccc99de500cda04d12c1
DongchannN <ldc991104@gmail.com>
2024-11-10 11:04:13 +0900

    feat: 사용자 구매 목록 관련 모델 구현

---
+++ src/main/java/store/model/PurchaseOrder.java
@@ -0,0 +1,37 @@
+package store.model;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class PurchaseOrder {
+    private final Map<String, Integer> purchaseOrder;
+
+    private PurchaseOrder(Map<String, Integer> purchaseOrder) {
+        this.purchaseOrder = new HashMap<>(purchaseOrder);
+    }
+
+    public static PurchaseOrder from(Map<String, Integer> items, StoreRoom storeRoom) {
+        validateHasProduct(items.keySet(), storeRoom);
+        validateHasStock(items, storeRoom);
+        return new PurchaseOrder(items);
+    }
+
+    private static void validateHasProduct(Set<String> productNames, StoreRoom storeRoom) {
+        List<String> nonExistProducts = productNames.stream()
+                .filter(productName -> !storeRoom.hasProduct(productName))
+                .toList();
+        if (!nonExistProducts.isEmpty()) {
+            throw new IllegalArgumentException("");
+        }
+    }
+
+    private static void validateHasStock(Map<String, Integer> products, StoreRoom storeRoom) {
+        products.forEach((name, quantity) -> {
+            if (!storeRoom.hasAvailableStock(name, quantity)) {
+                throw new IllegalArgumentException("");
+            }
+        });
+    }
+}
